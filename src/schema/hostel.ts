7a1b7ff3f305de16a0825aea91143da93f2865cc
vijaysb0613 <vijaysb2006@gmail.com>
2026-01-07 16:32:24 +0530

    Revert "fix/build fixes"
    This reverts commit 07feb2dac305b19c4f17d75204fcbdc20400de1c.

--- src/schema/hostel.ts
+++ src/schema/hostel.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const createHostelSchema = z.object({
     hostel_id: z.uuid().optional(),
     room_count: z.number().min(1, { message: "Available rooms must be at least 1" }),
-    room_filled: z.number().min(0, { message: "Filled rooms cannot be negative" }).default(0),
+    room_filled: z.number().min(0, { message: "Filled rooms cannot be negative" }),
     is_male: z.boolean(),
     warden_email: z.email({ message: "Invalid email address" }),
     latitude: z.string().optional(),
